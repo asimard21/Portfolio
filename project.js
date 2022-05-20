1988162a8fdea7fdda883c9a6720cd8fd01a45f2
asimard21 <100657046+asimard21@users.noreply.github.com>
2022-05-20 11:14:54 -0400

    Added interactity in project section. Can click to make description appear.

---
+++ project.js
@@ -0,0 +1,43 @@
+const desc1 = document.getElementById('projet1-description');
+const desc2 = document.getElementById('projet2-description');
+const desc3 = document.getElementById('projet3-description');
+const desc4 = document.getElementById('projet4-description');
+
+const img1 = document.getElementById('projet1-image');
+const img2 = document.getElementById('projet2-image');
+const img3 = document.getElementById('projet3-image');
+const img4 = document.getElementById('projet4-image');
+
+
+
+img1.onclick = () => {
+    if (desc1.style.visibility === 'visible') {
+        desc1.style.visibility = 'hidden';
+    } else {
+        desc1.style.visibility = 'visible';
+    }
+}
+
+img2.onclick = () => {
+    if (desc2.style.visibility === 'visible') {
+        desc2.style.visibility = 'hidden';
+    } else {
+        desc2.style.visibility = 'visible';
+    }
+}
+
+img3.onclick = () => {
+    if (desc3.style.visibility === 'visible') {
+        desc3.style.visibility = 'hidden';
+    } else {
+        desc3.style.visibility = 'visible';
+    }
+}
+
+img4.onclick = () => {
+    if (desc4.style.visibility === 'visible') {
+        desc4.style.visibility = 'hidden';
+    } else {
+        desc4.style.visibility = 'visible';
+    }
+}
