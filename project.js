5fbb9ff478823332852378806bdf8800908c9371
asimard21 <100657046+asimard21@users.noreply.github.com>
2022-05-29 00:11:54 -0400

    changed interactivity of project section

--- project.js
+++ project.js
@@ -1,53 +1,67 @@
+const descriptions = document.getElementsByClassName('full-desc');
+const clickMe = document.getElementsByClassName('clickMe');
+
 const desc1 = document.getElementById('projet1-description');
 const desc2 = document.getElementById('projet2-description');
 const desc3 = document.getElementById('projet3-description');
 const desc4 = document.getElementById('projet4-description');
 
-const img1 = document.getElementById('projet1-image');
-const img2 = document.getElementById('projet2-image');
-const img3 = document.getElementById('projet3-image');
-const img4 = document.getElementById('projet4-image');
+console.log(desc2);
 
 
-
-img1.onclick = () => {
+desc1.onclick = () => {
     if (window.innerWidth > 1000) {
-        if (desc1.style.visibility === 'visible') {
-            desc1.style.visibility = 'hidden';
+        if (clickMe[0].style.display === 'none') {
+            clickMe[0].style.display = 'block';
+            descriptions[0].style.display = 'none';    
         } else {
-            desc1.style.visibility = 'visible';
+            clickMe[0].style.display = 'none';
+            descriptions[0].style.display = 'block';
         }
+          
+    }
+} 
+
+desc2.onclick = () => {
+    if (window.innerWidth > 1000) {
+        if (clickMe[1].style.display === 'none') {
+            clickMe[1].style.display = 'block';
+            descriptions[1].style.display = 'none';    
+        } else {
+            clickMe[1].style.display = 'none';
+            descriptions[1].style.display = 'block';
+        }
+          
     }
 }
 
-img2.onclick = () => {
+desc3.onclick = () => {
     if (window.innerWidth > 1000) {
-        if (desc2.style.visibility === 'visible') {
-            desc2.style.visibility = 'hidden';
+        if (clickMe[2].style.display === 'none') {
+            clickMe[2].style.display = 'block';
+            descriptions[2].style.display = 'none';    
         } else {
-            desc2.style.visibility = 'visible';
+            clickMe[2].style.display = 'none';
+            descriptions[2].style.display = 'block';
         }
+          
     }
-}
+} 
 
-img3.onclick = () => {
+desc4.onclick = () => {
     if (window.innerWidth > 1000) {
-        if (desc3.style.visibility === 'visible') {
-            desc3.style.visibility = 'hidden';
+        if (clickMe[3].style.display === 'none') {
+            clickMe[3].style.display = 'block';
+            descriptions[3].style.display = 'none';    
         } else {
-            desc3.style.visibility = 'visible';
+            clickMe[3].style.display = 'none';
+            descriptions[3].style.display = 'block';
         }
+          
     }
-}
+} 
 
-img4.onclick = () => {
-    if (window.innerWidth > 1000) {
-        if (desc4.style.visibility === 'visible') {
-            desc4.style.visibility = 'hidden';
-        } else {
-            desc4.style.visibility = 'visible';
-        }
-    }
-   
-}
+
+
+
 
