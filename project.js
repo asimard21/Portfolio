f3608d964706ddeeebd6354accf527fc285d4df5
asimard21 <100657046+asimard21@users.noreply.github.com>
2022-05-30 10:04:25 -0400

    Added script to always show project descriptions for smaller screen

--- project.js
+++ project.js
@@ -1,16 +1,14 @@
-const descriptions = document.getElementsByClassName('full-desc');
-const clickMe = document.getElementsByClassName('clickMe');
+const descriptions = Array.from(document.getElementsByClassName('full-desc'));
+const clickMe = Array.from(document.getElementsByClassName('clickMe'));
+
 
 const desc1 = document.getElementById('projet1-description');
 const desc2 = document.getElementById('projet2-description');
 const desc3 = document.getElementById('projet3-description');
 const desc4 = document.getElementById('projet4-description');
 
-console.log(desc2);
-
-
-desc1.onclick = () => {
-    if (window.innerWidth > 1000) {
+if (window.innerWidth > 1000) {
+    desc1.onclick = () => {
         if (clickMe[0].style.display === 'none') {
             clickMe[0].style.display = 'block';
             descriptions[0].style.display = 'none';    
@@ -18,25 +16,17 @@ desc1.onclick = () => {
             clickMe[0].style.display = 'none';
             descriptions[0].style.display = 'block';
         }
-          
     }
-} 
-
-desc2.onclick = () => {
-    if (window.innerWidth > 1000) {
-        if (clickMe[1].style.display === 'none') {
-            clickMe[1].style.display = 'block';
-            descriptions[1].style.display = 'none';    
-        } else {
-            clickMe[1].style.display = 'none';
-            descriptions[1].style.display = 'block';
-        }
-          
+    desc2.onclick = () => {
+            if (clickMe[1].style.display === 'none') {
+                clickMe[1].style.display = 'block';
+                descriptions[1].style.display = 'none';    
+            } else {
+                clickMe[1].style.display = 'none';
+                descriptions[1].style.display = 'block';
+            }
     }
-}
-
-desc3.onclick = () => {
-    if (window.innerWidth > 1000) {
+    desc3.onclick = () => {
         if (clickMe[2].style.display === 'none') {
             clickMe[2].style.display = 'block';
             descriptions[2].style.display = 'none';    
@@ -44,12 +34,9 @@ desc3.onclick = () => {
             clickMe[2].style.display = 'none';
             descriptions[2].style.display = 'block';
         }
-          
     }
-} 
-
-desc4.onclick = () => {
-    if (window.innerWidth > 1000) {
+    desc4.onclick = () => {
+    
         if (clickMe[3].style.display === 'none') {
             clickMe[3].style.display = 'block';
             descriptions[3].style.display = 'none';    
@@ -57,11 +44,14 @@ desc4.onclick = () => {
             clickMe[3].style.display = 'none';
             descriptions[3].style.display = 'block';
         }
-          
-    }
-} 
+    } 
 
+}else {
+    clickMe.forEach((element) => {
+        element.style.display = 'none';
+    })
 
-
-
-
+    descriptions.forEach((element) => {
+        element.style.display = 'block';
+    }) 
+}
